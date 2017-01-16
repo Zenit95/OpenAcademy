83ab39a06a26ab61f408536d71d44e8127bc16d7
Zenit95 <javiermartineztejero@gmail.com>
2017-01-16 14:27:01 +0100

    Rubrica 19

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from . import controllers
-from . import models
+from controllers import controllers
+from models import models
--- models.py
+++ models.py
@@ -7,3 +7,8 @@ from openerp import models, fields, api
 
 #     name = fields.Char()
 
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
