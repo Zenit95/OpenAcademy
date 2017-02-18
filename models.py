ddd3d3dbdee39c63d877d50681b76775075ba25f
Zenit95 <javiermartineztejero@gmail.com>
2017-02-18 12:40:34 +0100

    Rubrica26

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from openerp import models, fields, api, exceptions
-from cookielib import domain_match
+from reportlab.graphics.renderbase import inverse
+from odoo.addons.gamification.models.challenge import start_end_date_for_period
 
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
@@ -54,6 +56,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor", 
         domain=['|', ('instructor', '=', True),
@@ -63,6 +66,14 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+    
+    hours = fields.Float(string="Duration in hours",
+                        compute='_get_hours', inverse='_set_hours')
+    
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
     
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -90,10 +101,48 @@ class Session(models.Model):
             }
 
 
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
             
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+            
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date) +1
+            
+    
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration *24
+            
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours/24
+            
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
+          
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
-        for n in self:
+        for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
                 raise exceptions.ValidationError("A session's instructor can't be an attendee") 
                
